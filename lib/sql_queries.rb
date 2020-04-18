d9d2c5a2f777efde8b9a268c5f8472d5c78bf58a
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-18 15:04:26 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -17,7 +17,7 @@ end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
   "SELECT pr.title, SUM(pl.amount)  FROM projects pr
-   JOIN pledges pl ON pl.project_id = pr.id GROUP BY titles HAVING SUM(pl.amount) >= pr.funding_goal;"
+   JOIN pledges pl ON pl.project_id = pr.id GROUP BY title HAVING SUM(pl.amount) >= pr.funding_goal;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
