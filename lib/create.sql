f07fd091dee90c10761833bc7a58dc42b4855acc
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-18 14:21:45 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -6,4 +6,20 @@ CREATE TABLE project
   funding_goal INTEGER,
   start_date TEXT,
   end_date TEXT
-)
+);
+
+
+CREATE TABLE user
+(
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER
+);
+
+CREATE TABLE pledge
+(
+  id INTEGER PRIMARY KEY,
+  amount INTEGER,
+  user_id INTEGER,
+  project_id INTEGER
+);
