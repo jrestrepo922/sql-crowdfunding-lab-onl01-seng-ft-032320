66491ed4aca1b150aadb25cd09276aa431751e0a
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-18 14:22:47 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,4 +1,4 @@
-CREATE TABLE project
+CREATE TABLE projects
 (
   id INTEGER PRIMARY KEY,
   title TEXT,
@@ -9,14 +9,14 @@ CREATE TABLE project
 );
 
 
-CREATE TABLE user
+CREATE TABLE users
 (
   id INTEGER PRIMARY KEY,
   name TEXT,
   age INTEGER
 );
 
-CREATE TABLE pledge
+CREATE TABLE pledges
 (
   id INTEGER PRIMARY KEY,
   amount INTEGER,
