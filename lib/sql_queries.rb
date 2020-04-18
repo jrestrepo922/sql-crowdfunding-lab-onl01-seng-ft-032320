7bea47a36fc20d12dd75f579452945b4b6503a6b
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-18 15:15:03 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -26,7 +26,8 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-  "Write your SQL query Here"
+  "SELECT pr.category, pl.amount FROM projects pr
+   JOIN pledges pl ON pl.project_id = pr.id"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
