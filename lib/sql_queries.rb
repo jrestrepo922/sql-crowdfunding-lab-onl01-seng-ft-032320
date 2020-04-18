431bde7b36ef54eda941c5fd685aefb7e7f0f360
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-18 15:12:50 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -21,8 +21,8 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
-  "SELECT u.name, SUM(u.amount) FROM users u
-   JOIN pledges pl ON pl.user_id = u.id GROUP BY u.name ORDER BY SUM(u.amount); "
+  "SELECT u.name, SUM(pl.amount) FROM users u
+   JOIN pledges pl ON pl.user_id = u.id GROUP BY u.name ORDER BY SUM(pl.amount); "
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
